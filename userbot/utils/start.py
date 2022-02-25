793b2d5ae645e7773aeb60b32811279e79828c9a
Xayzo <deonseorang@gmail.com>
2022-02-25 22:09:23 +0700

    Update start.py

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -11,9 +11,9 @@ async def startupmessage():
         if BOTLOG:
             await tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/02f87cca391f9b9d627d5.jpg",
-                caption="✨ **Kyy Userbot Has Been Actived**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @NastyProject ",
-                buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/NastySupportt"),)],
+                "https://telegra.ph/file/5691a1f2ab21ffb310b94.jpg",
+                caption="✨ **Galon-Userbot Has Been Actived**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @GalonUpdates ",
+                buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/GalonSupport"),)],
             )
     except Exception as e:
         LOGS.error(e)
